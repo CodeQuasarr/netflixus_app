db17f7499f763025c1a42e5bd0e03e2a0617e409
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-07-04 06:03:33 +0200

    mis en place de pinia

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import './main.css'
 import 'flowbite';
 import App from './App.vue'
 import router from "./routes";
+import {createPinia} from "pinia";
+
+const pinia = createPinia()
 
 createApp(App)
+    .use(pinia)
     .use(router)
     .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), svgLoader()],
+    define: {
+        'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+        'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+        'process.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID)
+    },
     server: {
         port: 3000,
         open: true,
