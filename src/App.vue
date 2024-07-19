a5d3326b68a7c90a0b1a4eeae7713c180273cbbd
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-07-19 22:58:35 +0200

    debug composant de yeald

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 
-const apiKey = import.meta.env.VITE_VUE_APP_API_KEY;
-const baseUrl = import.meta.env.VITE_VUE_APP_BASE_URL;
 </script>
 
 <template>
   <div>
-      <h1 class="text-3xl font-bold underline bg-red-600">
-          Hello world! {{ apiKey }} {{ baseUrl }}
-      </h1>
+      <RouterView />
   </div>
 </template>
 
