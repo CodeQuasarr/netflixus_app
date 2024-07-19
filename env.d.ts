62e52b1df361c2de56090b2352a4b7be86d81a3e
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-07-19 22:33:45 +0200

    bug 3 init .env

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VUE_APP_API_KEY: string;
+    readonly VUE_APP_BASE_URL: string;
+    // Ajoutez d'autres variables d'environnement ici si nécessaire
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
