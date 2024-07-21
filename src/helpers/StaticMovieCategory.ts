b9aca1c8c3367dad197f7982b33821d76489501b
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-07-21 16:57:12 +0200

    push

--- src/helpers/StaticMovieCategory.ts
+++
@@ -1,8 +0,0 @@
-export const movieCategories = [
-    { id: 28,     name: "Action",           label: "action" },
-    { id: 12,     name: "Aventure",         label: "aventure" },
-    { id: 35,     name: "Comédie",          label: "comédie" },
-    { id: 27,     name: "Horreur",          label: "horreur" },
-    { id: 10402,  name: "Musique",          label: "musique" },
-    { id: 878,    name: "Science-Fiction",  label: "science_fiction" },
-]
